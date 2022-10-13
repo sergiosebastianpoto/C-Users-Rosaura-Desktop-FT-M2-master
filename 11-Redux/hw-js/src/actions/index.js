9c6ddf843786b1e20ff122c61cda8433ed25bc53
sergiosebastianpoto <sergiosebastianpoto@gmail.com>
2022-10-13 12:42:46 -0300

    react

--- 11-Redux/hw-js/src/actions/index.js
+++ 11-Redux/hw-js/src/actions/index.js
@@ -1,15 +1,27 @@
-const { INCREMENTO, DECREMENTO } = require('../action-types');
+const {
+  isPrematureTemplateContentError,
+} = require("@11ty/eleventy/src/EleventyErrorUtil");
+const { INCREMENTO, DECREMENTO } = require("../action-types");
 
-// Nuestras actions (action creators) devolverán un paquete de actions que nuestro reducer recibirá. 
-// ¿Cómo es el paquete de acción? Tengan en cuenta que el creador de la acción no es en absoluto responsable 
+// Nuestras actions (action creators) devolverán un paquete de actions que nuestro reducer recibirá.
+// ¿Cómo es el paquete de acción? Tengan en cuenta que el creador de la acción no es en absoluto responsable
 // de manejar ninguna de las lógicas actuales de actualización del store central de Redux.
 // Eso se lo deja al reducer(s).
 
-const incremento = null;
+export const incremento = () => {
+  // completa la funcion
+  return {
+    type: "INCREMENTO",
+  };
+};
 
-const decremento = null;
+const decremento = () => {
+  return {
+    type: "DECREMENTO",
+  };
+};
 
 module.exports = {
   incremento,
-  decremento
-}
+  decremento,
+};
